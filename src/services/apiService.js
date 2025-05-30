c0d6c7dec3c69c7df6fe46bc189384c005acd69c
Renzo Arias <rariasbarrero@gmail.com>
2025-05-30 03:22:53 -0500

    agregando variables de entorno

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,52 +1,52 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const headers = {
+  'Content-Type': 'application/json',
+  'ngrok-skip-browser-warning': 'true'
+};
 
 export async function fetchOpciones(endpoint) {
-    const res = await fetch(`${BASE_URL}/${endpoint}`);
-    const data = await res.json();
-    return Array.isArray(data) ? data : data[endpoint];
+  const res = await fetch(`${BASE_URL}/${endpoint}`, { headers });
+  const data = await res.json();
+  return Array.isArray(data) ? data : data[endpoint];
 }
 
 export async function agregarSede(sede, force = false) {
-    const response = await fetch(`${BASE_URL}/sedes`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ sede, force }),
-    });
+  const response = await fetch(`${BASE_URL}/sedes`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ sede, force }),
+  });
 
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'Error agregando sede');
-    }
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Error agregando sede');
+  }
 }
-
 
 export async function agregarProyecto(proyecto, force = false) {
-    const response = await fetch(`${BASE_URL}/proyectos`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ proyecto, force }),
-    });
+  const response = await fetch(`${BASE_URL}/proyectos`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ proyecto, force }),
+  });
 
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'Error al agregar el proyecto');
-    }
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Error al agregar el proyecto');
+  }
 }
 
-
-
 export async function enviarRegistro(data) {
-    await fetch(`${BASE_URL}/registro`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    });
+  await fetch(`${BASE_URL}/registro`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(data),
+  });
 }
 
 export async function fetchOpcionesFiltrado(endpoint, query) {
-    const res = await fetch(`${BASE_URL}/${endpoint}?q=${encodeURIComponent(query)}`);
-    const data = await res.json();
-    return data[endpoint];
+  const res = await fetch(`${BASE_URL}/${endpoint}?q=${encodeURIComponent(query)}`, { headers });
+  const data = await res.json();
+  return data[endpoint];
 }
-
-
